e798ac2e60cc2ec440dbfb221d10be76c4231952
George K <93683612+GeorgeK04@users.noreply.github.com>
2021-11-22 14:54:08 +0200

    Add files via upload

---
+++ Musify/src/Favourites.java
@@ -0,0 +1,113 @@
+package Login;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class Favourites {
+
+	private JFrame frame;
+	private JTable tableFavourites;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Favourites window = new Favourites();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Favourites() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.GRAY);
+		frame.setBounds(100, 100, 698, 536);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblLogo = new JLabel("");
+		lblLogo.setIcon(new ImageIcon("C:\\Users\\KYVOS\\eclipse-workspace\\Login\\src\\Images\\musifyLogoNew.png"));
+		lblLogo.setBounds(124, 11, 439, 50);
+		frame.getContentPane().add(lblLogo);
+		
+		
+		//button back
+		JButton btnBack = new JButton("Back");
+		btnBack.setFont(new Font("Dubai", Font.PLAIN, 11));
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				FormMusify.main(null);
+			}
+		});
+		btnBack.setBounds(27, 453, 89, 33);
+		frame.getContentPane().add(btnBack);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
+		panel.setBounds(10, 109, 662, 336);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		tableFavourites = new JTable();
+		tableFavourites.setBounds(10, 11, 642, 314);
+		panel.add(tableFavourites);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.DARK_GRAY);
+		panel_1.setBounds(244, 60, 173, 38);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Your Favourite Songs List");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Dubai", Font.BOLD, 11));
+		lblNewLabel.setForeground(Color.BLACK);
+		lblNewLabel.setBackground(Color.BLACK);
+		lblNewLabel.setBounds(10, 11, 153, 14);
+		panel_1.add(lblNewLabel);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(10, 11, 152, 16);
+		panel_1.add(panel_2);
+		panel_2.setLayout(null);
+		
+		//button Close
+		JButton btnClose = new JButton("Close");
+		btnClose.setFont(new Font("Dubai", Font.PLAIN, 11));
+		btnClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnClose.setBounds(583, 453, 89, 33);
+		frame.getContentPane().add(btnClose);
+	}
+}
---
+++ Musify/src/Playlist.java
@@ -0,0 +1,126 @@
+package Login;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Image;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+
+public class Playlist {
+
+	private JFrame frame;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Playlist window = new Playlist();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	
+		
+		
+		
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Playlist() {
+		initialize();
+	}
+	
+
+	
+	
+
+
+	
+	
+	
+	
+
+	 // Initialize the contents of the frame.
+	 
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.GRAY);
+		frame.setBounds(100, 100, 749, 618);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	    
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.DARK_GRAY);
+		panel_1.setBounds(250, 11, 282, 46);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(10, 11, 262, 27);
+		panel_1.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Your Playlist");
+		lblNewLabel_1.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBackground(Color.MAGENTA);
+		lblNewLabel_1.setFont(new Font("Dubai", Font.BOLD, 13));
+		lblNewLabel_1.setBounds(10, 5, 242, 22);
+		lblNewLabel_1.setToolTipText("");
+		panel.add(lblNewLabel_1);
+		
+		JPanel panel_1_1 = new JPanel();
+		panel_1_1.setLayout(null);
+		panel_1_1.setBackground(Color.DARK_GRAY);
+		panel_1_1.setBounds(10, 11, 230, 400);
+		frame.getContentPane().add(panel_1_1);
+		
+		JLabel img = new JLabel("");
+		img.setHorizontalAlignment(SwingConstants.CENTER);
+		img.setBounds(10, 11, 210, 378);
+		panel_1_1.add(img);
+		img.setIcon(new ImageIcon("C:\\Users\\KYVOS\\eclipse-workspace\\Login\\src\\Images\\logo.png"));
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setFont(new Font("Dubai", Font.PLAIN, 11));
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				FormMusify.main(null);
+			}
+		});
+		btnBack.setBackground(Color.WHITE);
+		btnBack.setBounds(10, 534, 89, 34);
+		frame.getContentPane().add(btnBack);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.DARK_GRAY);
+		panel_2.setBounds(250, 68, 282, 460);
+		frame.getContentPane().add(panel_2);
+		panel_2.setLayout(null);
+		
+		table = new JTable();
+		table.setBounds(10, 11, 262, 438);
+		panel_2.add(table);
+	}
+
+}
