e2bf834e5f9c9f6f86649279800dedb7b8ac2455
George Kofidis <87643477+GeorgeKofidis@users.noreply.github.com>
2021-11-24 18:15:10 +0200

    Update Admin.java

--- musifyApp/src/main/java/Connection/Admin.java
+++ musifyApp/src/main/java/Connection/Admin.java
@@ -52,7 +52,7 @@ public class Admin {
 				try {
 					
 					//Class.forName("com.mysql.jdbc.Driver");
-					Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","Sarap4610_Kof4665_Ioan4578_Alex4631");
+					Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","");
 					
 					String username = userText.getText();
 					String password = String.valueOf(passText.getPassword());
