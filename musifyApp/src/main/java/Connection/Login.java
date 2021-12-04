d4d19f975f7d1b60b4010587f858e191aca173ef
George K <93683612+GeorgeK04@users.noreply.github.com>
2021-12-05 01:42:14 +0200

    Update Login.java

--- musifyApp/src/main/java/Connection/Login.java
+++ musifyApp/src/main/java/Connection/Login.java
@@ -103,7 +103,7 @@ public class Login{
 				try {
 					
 					//Class.forName("com.mysql.jdbc.Driver");
-					Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","giwrgos2131");
+					Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","");
 					
 					String username = textFieldUserN.getText();
 					String password = String.valueOf(passwordField.getPassword());
