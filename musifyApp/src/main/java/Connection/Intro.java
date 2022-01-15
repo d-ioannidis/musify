542bd9118b05af3337377b16f0b4cf456d0f517e
George K <93683612+GeorgeK04@users.noreply.github.com>
2022-01-15 18:39:58 +0200

    FormIntro new design

--- musifyApp/src/main/java/Connection/Intro.java
+++ musifyApp/src/main/java/Connection/Intro.java
@@ -43,7 +43,7 @@ public class Intro {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+		frame.getContentPane().setBackground(Color.DARK_GRAY);
 		frame.setBackground(Color.GRAY);
 		frame.getContentPane().setForeground(Color.GRAY);
 		frame.setBounds(100, 100, 450, 386);
@@ -51,29 +51,26 @@ public class Intro {
 		frame.getContentPane().setLayout(null);
 		
 		JButton btnUserLogin = new JButton("Log in as User");
+		btnUserLogin.setIcon(new ImageIcon("C:\\Users\\KYVOS\\eclipse-workspace\\musify-main\\musifyApp\\src\\main\\java\\buttons\\login.png"));
 		btnUserLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame.dispose();
                 Login.main(null);
 			}
 		});
-		btnUserLogin.setBounds(114, 159, 199, 44);
+		btnUserLogin.setBounds(114, 116, 199, 44);
 		frame.getContentPane().add(btnUserLogin);
 		
 		JButton btnAdminLogin = new JButton("Log in as Admin");
+		btnAdminLogin.setIcon(new ImageIcon("C:\\Users\\KYVOS\\eclipse-workspace\\musify-main\\musifyApp\\src\\main\\java\\buttons\\login.png"));
 		btnAdminLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame.dispose();
                 Admin.main(null);
 			}
 		});
-		btnAdminLogin.setBounds(114, 235, 199, 44);
+		btnAdminLogin.setBounds(114, 186, 199, 44);
 		frame.getContentPane().add(btnAdminLogin);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon(Intro.class.getResource("/Images/musifyLogoNew.png")));
-		lblNewLabel.setBounds(-47, 11, 425, 107);
-		frame.getContentPane().add(lblNewLabel);
 		
 		JButton btnClose = new JButton("");
 		btnClose.setIcon(new ImageIcon(Intro.class.getResource("/buttons/close.png")));
@@ -82,7 +79,13 @@ public class Intro {
 				frame.dispose();
 			}
 		});
-		btnClose.setBounds(10, 292, 59, 44);
+		btnClose.setBounds(10, 292, 44, 44);
 		frame.getContentPane().add(btnClose);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(Intro.class.getResource("/Images/logoMain.png")));
+		lblNewLabel_1.setBounds(0, 0, 434, 347);
+		frame.getContentPane().add(lblNewLabel_1);
 	}
 }
+
